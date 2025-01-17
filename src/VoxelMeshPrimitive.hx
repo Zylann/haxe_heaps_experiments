80b2612f14db14777adaf7e41a17fc5e748f2edf
Marc Gilleron <marc.gilleron@gmail.com>
2025-01-17 21:29:37 +0000

    Better names

--- src/VoxelMeshPrimitive.hx
+++ src/VoxelMeshPrimitive.hx
@@ -1,21 +1,21 @@
 import h3d.col.Bounds;
 
 class VoxelMeshPrimitive extends h3d.prim.MeshPrimitive {
-	var fbuffer:hxd.FloatBuffer;
-	var ibuffer:hxd.IndexBuffer;
+	var vertexBuffer:hxd.FloatBuffer;
+	var indexBuffer:hxd.IndexBuffer;
 	var bounds:Bounds;
 
-	public function new(fbuffer:hxd.FloatBuffer, ibuffer:hxd.IndexBuffer, bounds:h3d.col.Bounds) {
-		this.fbuffer = fbuffer;
-		this.ibuffer = ibuffer;
+	public function new(vertexBuffer:hxd.FloatBuffer, indexBuffer:hxd.IndexBuffer, bounds:h3d.col.Bounds) {
+		this.vertexBuffer = vertexBuffer;
+		this.indexBuffer = indexBuffer;
 		this.bounds = bounds;
 	}
 
 	override function alloc(engine:h3d.Engine) {
 		dispose();
 		var format = hxd.BufferFormat.POS3D_NORMAL_UV;
-		buffer = h3d.Buffer.ofFloats(fbuffer, format);
-		indexes = h3d.Indexes.alloc(ibuffer);
+		buffer = h3d.Buffer.ofFloats(vertexBuffer, format);
+		indexes = h3d.Indexes.alloc(indexBuffer);
 	}
 
 	override function getBounds():Bounds {

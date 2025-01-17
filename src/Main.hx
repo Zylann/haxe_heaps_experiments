dce8d18b257007a0830a198f21f02a4f4d3e7b16
Marc Gilleron <mgilleron@freejamgames.com>
2025-01-17 17:12:38 +0000

    Move generator to its own file

--- src/Main.hx
+++ src/Main.hx
@@ -1,4 +1,3 @@
-import hxd.Perlin;
 import hxd.Timer;
 import h3d.scene.fwd.DirLight;
 import h3d.scene.Mesh;
@@ -44,7 +43,7 @@ class Main extends hxd.App {
 					var originX = cx * chunkSize;
 					var originY = cy * chunkSize;
 					var originZ = cz * chunkSize;
-					generateChunkVoxels(voxels, originX - Mesher.PAD, originY - Mesher.PAD, originZ - Mesher.PAD);
+					ChunkGenerator.generateChunkVoxels(voxels, originX - Mesher.PAD, originY - Mesher.PAD, originZ - Mesher.PAD);
 					var voxelPrim = Mesher.build(voxels);
 					var voxelMesh = new Mesh(voxelPrim, s3d);
 					voxelMesh.setPosition(originX, originY, originZ);
@@ -55,44 +54,6 @@ class Main extends hxd.App {
 
 		var camera = s3d.camera;
 		camera.fovY = 80;
-	}
-
-	static function generateChunkVoxels(voxels:VoxelBuffer, originX:Int, originY:Int, originZ:Int) {
-		var perlin = new Perlin();
-		var seed = 131183;
-
-		// TODO Apparently hxd.Perlin returns garbage if any coordinate is negative.
-		// This is not good for infinite terrain usage.
-		var noiseOffsetX = 1000.0;
-		var noiseOffsetY = 1000.0;
-
-		for (ry in 0...voxels.sizeY) {
-			var y = originY + ry;
-			for (rx in 0...voxels.sizeX) {
-				var x = originX + rx;
-
-				var n1 = 40.0 * perlin.perlin(seed, x * 0.01 + noiseOffsetX, y * 0.01 + noiseOffsetY, 4);
-				var n2 = perlin.perlin(seed + 1, x * 0.02 + noiseOffsetX, y * 0.02 + noiseOffsetX, 3);
-
-				for (rz in 0...voxels.sizeZ) {
-					var z = originZ + rz;
-
-					// var sd = z - 10 + 4.0 * (Math.sin(x * 0.2) + Math.cos(y * 0.2));
-					var sd = z - 10 + n1;
-
-					if (sd < 0.0) {
-						var b = 1;
-						if (n2 > 0.1) {
-							b = 2;
-						}
-
-						voxels.setVoxel(rx, ry, rz, b);
-					} else {
-						voxels.setVoxel(rx, ry, rz, 0);
-					}
-				}
-			}
-		}
 	}
 
 	override function update(dt:Float) {
